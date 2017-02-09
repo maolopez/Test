66e7e827054b8ed1bd0c9eaddc8c2d753593991f
maolopez <maolopez@users.noreply.github.com>
2017-02-08 20:55:47 -0500

    Create monitoranyhttp.sh

---
+++ monitoranyhttp.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+location=$1
+
+If  [ -z "$location" ]
+then
+         echo "please provide the location argument without http://"
+         exit
+fi
+
+res=`curl -s -L -I $location | grep HTTP/1.1 | awk '/200/ {count++} END{print count}'`
+
+if [ "$res" = "0" ]
+then
+mkdir /home/user/scripts/$location
+cd /home/user/scripts/$location
+wget -drc --tries=2 http://$location/ -o log
+cat /home/user/scripts/$location | grep -i "error" | echo "ACTION REQUIRED: $location is down" 
+fi
+
+echo "DONE! $res"
